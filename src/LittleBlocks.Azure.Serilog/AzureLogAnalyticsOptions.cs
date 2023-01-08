e354aeacaf4fe37545953e8835bf734a77cdc044
Mohammad Moattar <moattarwork@msn.com>
2023-01-08 19:45:46 +0000

    Feature/port from easify (#1)
    * Initial port of the code
    
    * Prepare project for the first release
    
    * Upgade the packages to the latest
    
    * Replace the builds
    
    * Added missing icons and configurations
    
    * Added the icon to the project
    
    * Remove the logo from serilog
    
    * Remove the logo
    
    * Update the project files to the right project targets
    
    * Remove logo from all projects due to the error on build

--- src/LittleBlocks.Azure.Serilog/AzureLogAnalyticsOptions.cs
+++ src/LittleBlocks.Azure.Serilog/AzureLogAnalyticsOptions.cs
@@ -1,10 +1,11 @@
-﻿namespace Easify.Azure.Serilog
+﻿namespace LittleBlocks.Azure.Serilog
 {
     public sealed class AzureLogAnalyticsOptions
     {
         public string WorkspaceId { get; set; }
         public string AuthenticationId { get; set; }
         public string LogName { get; set; }
+        
         public int? LogBufferSize { get; set; }
         public int? BatchSize { get; set; }
     }
