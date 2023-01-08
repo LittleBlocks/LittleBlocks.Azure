67b302e7f1fc8206a67c129803f2cd156483646d
moattarwork <moattarwork@msn.com>
2023-01-08 18:55:22 +0000

    Replace the builds

--- src/LittleBlocks.Azure.Serilog/AzureLogAnalyticsOptions.cs
+++ src/LittleBlocks.Azure.Serilog/AzureLogAnalyticsOptions.cs
@@ -5,6 +5,7 @@
         public string WorkspaceId { get; set; }
         public string AuthenticationId { get; set; }
         public string LogName { get; set; }
+        
         public int? LogBufferSize { get; set; }
         public int? BatchSize { get; set; }
     }
